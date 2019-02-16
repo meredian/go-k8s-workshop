8b14e87b38d58cef905d7d4d254f5749d9a3fcd3
Anton Sidelnikov <ndmeredian@gmail.com>
2019-02-16 14:35:03 +0700

    Start tracking server

--- cmd/tracking/main.go
+++ cmd/tracking/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"gitlab.k8s.gromnsk.ru/workshop/austin/pkg/config"
+	"gitlab.k8s.gromnsk.ru/workshop/austin/pkg/tracking"
 )
 
 func main() {
 	fmt.Println("Tracking workshop")
+	cfg, err := config.GetConfig()
+	if err != nil {
+		log.Fatalf("filet to get config: %v\n", err)
+	}
+
+	tracking.Run(cfg)
 }
